e31deca4d1f0fb5ec884192aa7884705c4ff8be8
greg <gregc.sg@googlemail.com>
2015-06-29 22:37:47 +0100

    beebot constructor now has location

---
+++ runSciBot.py
@@ -0,0 +1,3 @@
+from src.BeeBot import *
+
+x = BeeBot()
---
+++ src/BeeBot.py
@@ -0,0 +1,4 @@
+class BeeBot:
+	def __init__(self,startPositionX=0,startPositionY=0):
+		locationX=startPositionX
+		locationY=startPositionY
