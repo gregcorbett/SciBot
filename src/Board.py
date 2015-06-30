77ed139a3b37c31c69b00cbe6116acf4016870a8
greg <gregc.sg@googlemail.com>
2015-06-30 20:22:55 +0100

    grid auto generated

--- src/Board.py
+++ src/Board.py
@@ -3,6 +3,7 @@ import sys
 
 class Board:
 	def __init__(self,height,width,step):
+		self.step = step
 		self.height = height*step
 		self.width = width*step
 		self.background=pygame.image.load("./img/background.jpg")
@@ -18,9 +19,28 @@ class Board:
 			print("Board Width = ",self.width)
 			print("Image Width = ",self.background.get_width())
 			sys.exit()
+		if ((self.height % self.step) != 0):
+			print("Error 3: height % step != 0")
+			print("Height = ",self.height)
+			print("Step   = ",self.step)
+			sys.exit()
+		if ((self.width % self.step) != 0):
+			print("Error 4: width % step != 0")
+			print("Width = ",self.width)
+			print("Step  = ",self.step)
+			sys.exit()
 		
 	def display(self,screen):
 		screen.blit(self.background,(0,0))
-		pygame.draw.line(screen,self.borderColour,(60,60),(120,120),4)
+		
+		iterWidth = 0
+		while (iterWidth <= self.width):
+			pygame.draw.line(screen,self.borderColour,(iterWidth,0),(iterWidth,self.width),5)
+			iterWidth = iterWidth + self.step
+		
+		iterHeight = 0
+		while (iterHeight <= self.height):
+			pygame.draw.line(screen,self.borderColour,(0,iterHeight),(self.width,iterHeight),5)
+			iterHeight = iterHeight + self.step
 		
 		pygame.display.update()
