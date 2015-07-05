e6f0bd36caf34bb3e966466c21b16e9f111f4d9e
greg <gregc.sg@googlemail.com>
2015-07-05 18:49:04 +0100

    another attempt at threading, program still crashes

--- runSciBot.py
+++ runSciBot.py
@@ -2,27 +2,27 @@ from src.GameWindow import *
 import sys
 import threading
 
-class updater:
-	def run():
-		
-		print("BOO")
-		
-		clock = pygame.time.Clock() 
-		
-		while True:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					pygame.quit()
-					sys.exit()
-			
-				clock.tick(30)
-				pygame.display.update()
-
-thr = threading.Thread(target=updater, args=(), kwargs={})
-thr.start() # will run "updateLoop forever 
+#pygame.init()
 
 gameWindow = GameWindow(8,5)
+
+#thr = threading.Thread(target=gameWindow.updateLoop(), args=(), kwargs={})
+#thr.daemon = True
+#thr.start() # will run "updateLoop forever 
+
+gameWindow.start()
 gameWindow.startInstance()
+
+#pygame.quit()
+#sys.exit()
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+
+
 #x = BeeBot()
 
 print("we got here")
--- src/GameWindow.py
+++ src/GameWindow.py
@@ -1,11 +1,15 @@
 import pygame
 import time
+import threading
 
 from src.BeeBot import *
 from src.Board import *
 
-class GameWindow:
+class GameWindow(threading.Thread):
 	def __init__(self,logicalHeight,logicalWidth):
+		pygame.init()
+		super(GameWindow,self).__init__()
+		
 		self.step = 150
 		
 		self.height = logicalHeight*self.step
@@ -15,7 +19,7 @@ class GameWindow:
 		self.size = (self.width,self.height)
 		
 		self.robot = BeeBot(6,3,self.board,Heading.NORTH)
-		pygame.init()
+		
 		self.screen = pygame.display.set_mode(self.size) 
 		
 		self.board.display(self.screen)
@@ -26,3 +30,12 @@ class GameWindow:
 	
 	def newGame(self):
 		self.robot.move(self.screen)
+		
+	def run(self):
+			clock = pygame.time.Clock()
+			while True:
+				self.board.display(self.screen)
+				self.robot.display(self.screen)
+				pygame.display.update()
+				#time.sleep(0.2)
+				#clock.tick(30)
