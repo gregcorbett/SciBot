99ccfd09da9613405e3bc003f72bd9d0d24d0a15
greg <gregc.sg@googlemail.com>
2015-07-20 21:58:35 +0100

    back to default scenario for now

--- src/GameWindow.py
+++ src/GameWindow.py
@@ -16,7 +16,7 @@ class GameWindow():
 		self.scenario = None
 		#Somehow choose a scenario
 		
-		self.scenario = "UV"
+		#self.scenario = "UV"
 		
 		if self.scenario == None:
 			self.scenario = "Default"
