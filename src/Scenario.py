c257804a5949ff268a773d15de90f5fad106904e
greg <scibot.dev@gmail.com>
2016-01-30 11:25:44 +0000

    scenario elements are now all stored within elements dict

--- src/Scenario.py
+++ src/Scenario.py
@@ -12,112 +12,117 @@ class Scenario():
 		self.elements = {}
 		self.name = name
 		self.elements['Name'] = name
-		
-		self.switcher = {
-			'Name': self.elements['Name'],
-		}
-		
-		self.boardStep = 0
-		self.logicalWidth = 0
-		self.logicalHeight = 0
-		
-		self.background = None
-		self.borderColour = None
-		
-		self.beeBotStartPosition = None
-		self.beeBotSprite = None
-		self.beeBotHeading = None
-		
-		self.obstacleGroup = {}
-		self.obstacleCount = 0
-
-		self.goalGroup = {}
-		self.goalCount = 0
-		
-		self.beeBotFailSprite = None
-		
+			
 	def get_element(self,key):
 	
 		if key not in self.elements:
 			return None
-			
-		return self.switcher.get(key)
+		
+		switcher = {
+			'Name': self.getName(),
+			'BoardStep': self.getBoardStep(),
+			'LogicalWidth': self.getLogicalWidth(),
+			'LogicalHeight': self.getLogicalHeight(),
+			'Background': self.getBackground(),
+			'BorderColour': self.getBorderColour(),
+			'BeeBotSprite': self.getBeeBotSprite(),
+			'BeeBotStartPosition': self.getBeeBotStartPosition(),
+			'BeeBotHeading': self.getBeeBotHeading(),
+			'ObstacleGroup': self.getObstacleGroup(),
+			'ObstacleCount': self.getObstacleCount(),
+			'GoalGroup': self.getGoalGroup(),
+			'GoalCount': self.getGoalCount(),
+			'BeeBotFailSprite': self.getBeeBotFailSprite(),
+		}
+		
+		return switcher.get(key)
 		
 	def setBeeBotFailSprite(self,inputString):
 		input = pygame.image.load(inputString)
-		self.beeBotFailSprite = self.formatSurfaceForPickle(input)
+		self.elements['BeeBotFailSprite'] = self.formatSurfaceForPickle(input)
 		
 	def getBeeBotFailSprite(self):
-		return self.formatPickleToSurface(self.beeBotFailSprite)
+		return self.formatPickleToSurface(self.elements['BeeBotFailSprite'])
 	
 	def addGoal(self,x,y,sprite=None):
+	
+		if 'GoalGroup' not in self.elements:
+			self.elements['GoalGroup'] = {}
+			self.elements['GoalCount'] = 0
+			
 		if sprite != None:
 			sprite = pygame.image.load(sprite)
 			sprite = self.formatSurfaceForPickle(sprite)
-		self.goalGroup[self.goalCount] = (sprite,x,y)
-		self.goalCount = self.goalCount + 1
+
+		self.elements['GoalGroup'][self.elements['GoalCount']] = (sprite,x,y)
+		self.elements['GoalCount'] = self.elements['GoalCount'] + 1
 		
 	def getGoalGroup(self):
 		goalGroup = GoalGroup()
 		goalPtr = 0
-		while goalPtr < self.goalCount:
-			pickledGoal = self.goalGroup[goalPtr]
-			goalGroup.add(Goal(self.formatPickleToSurface(pickledGoal[0]),pickledGoal[1],pickledGoal[2],self.boardStep))
+		while goalPtr < self.elements['GoalCount']:
+			pickledGoal = self.elements['GoalGroup'][goalPtr]
+			goalGroup.add(Goal(self.formatPickleToSurface(pickledGoal[0]),pickledGoal[1],pickledGoal[2],self.elements['BoardStep']))
 			goalPtr = goalPtr + 1
 		return goalGroup
 
 	def addObstacle(self,x,y,sprite=None):
+	
+		if 'ObstacleGroup' not in self.elements:
+			self.elements['ObstacleGroup'] = {}
+			self.elements['ObstacleCount'] = 0
+			
 		if sprite != None:
 			sprite = pygame.image.load(sprite)
 			sprite = self.formatSurfaceForPickle(sprite)
-		self.obstacleGroup[self.obstacleCount] = (sprite,x,y)
-		self.obstacleCount = self.obstacleCount + 1
+		self.elements['ObstacleGroup'][self.elements['ObstacleCount']] = (sprite,x,y)
+		self.elements['ObstacleCount'] = self.elements['ObstacleCount'] + 1
 	
 	def getObstacleGroup(self):
 		obstacleGroup = ObstacleGroup()
 		obsPtr = 0
-		while obsPtr < self.obstacleCount:
-			pickledObs = self.obstacleGroup[obsPtr]
-			obstacleGroup.add(Obstacle(self.formatPickleToSurface(pickledObs[0]),pickledObs[1],pickledObs[2],self.boardStep))
+		while obsPtr < self.elements['ObstacleCount']:
+			pickledObs = self.elements['ObstacleGroup'][obsPtr]
+			obstacleGroup.add(Obstacle(self.formatPickleToSurface(pickledObs[0]),pickledObs[1],pickledObs[2],self.elements['BoardStep']))
 			obsPtr = obsPtr + 1
 		return obstacleGroup
 		
 	def setBeeBotHeading(self,input):
-		self.beeBotHeading = input
+		self.elements['BeeBotHeading'] = input
 
 	def getBeeBotHeading(self):
-		return self.beeBotHeading
+		return self.elements['BeeBotHeading']
 		
 	def setBeeBotSprite(self,inputString):
 		input = pygame.image.load(inputString)
-		self.beeBotSprite = self.formatSurfaceForPickle(input)
+		self.elements['BeeBotSprite'] = self.formatSurfaceForPickle(input)
 		
 	def getBeeBotSprite(self):
-		return self.formatPickleToSurface(self.beeBotSprite)
+		return self.formatPickleToSurface(self.elements['BeeBotSprite'])
 		
 	def getBeeBotStartPosition(self):
-		return self.beeBotStartPosition
+		return self.elements['BeeBotStartPosition']
 		
 	def setBeeBotStartPosition(self,x,y):
-		self.beeBotStartPosition = (x,y)
+		self.elements['BeeBotStartPosition'] = (x,y)
 	
 	def getLogicalHeight(self):
-		return self.logicalHeight
+		return self.elements['LogicalHeight']
 		
 	def getLogicalWidth(self):
-		return self.logicalWidth	
+		return self.elements['LogicalWidth']	
 
 	def setLogicalHeight(self,input):
-		self.logicalHeight = input
+		self.elements['LogicalHeight'] = input
 		
 	def setLogicalWidth(self,input):
-		self.logicalWidth = input
+		self.elements['LogicalWidth'] = input
 		
 	def getBoardStep(self):
-		return self.boardStep
+		return self.elements['BoardStep']
 		
 	def setBoardStep(self,input):
-		self.boardStep = input
+		self.elements['BoardStep'] = input
 	
 	def formatPickleToSurface(self,input):
 		if input == None:
@@ -131,16 +136,16 @@ class Scenario():
 		
 	def setBackground(self,inputString):
 		input = pygame.image.load(inputString)
-		self.background = self.formatSurfaceForPickle(input)
+		self.elements['Background'] = self.formatSurfaceForPickle(input)
 		
 	def getBackground(self):
-		return self.formatPickleToSurface(self.background)
+		return self.formatPickleToSurface(self.elements['Background'])
 		
 	def setBorderColour(self,input):
-		self.borderColour = input
+		self.elements['BorderColour'] = input
 	
 	def getBorderColour(self):
-		return self.borderColour
+		return self.elements['BorderColour']
 	
 	def getName(self):
 		return self.elements['Name']
