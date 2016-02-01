1834bbc3aeb1a6b3bb9ee8e5e2f749b3489ba984
greg <scibot.dev@gmail.com>
2016-02-01 20:24:29 +0000

    comments added

--- src/CustomEvent.py
+++ src/CustomEvent.py
@@ -6,10 +6,12 @@ from pygame import USEREVENT
 class CustomEvent(IntEnum):
     """A class of IntEnums for locally defined events, such as RUN_FAIL."""
 
+    # BeeBot movement events
     MOVE_BEEBOT_UP = USEREVENT + 1
     MOVE_BEEBOT_LEFT = USEREVENT + 2
     MOVE_BEEBOT_DOWN = USEREVENT + 3
     MOVE_BEEBOT_RIGHT = USEREVENT + 4
 
+    # Win / Fail events
     RUN_FAIL = USEREVENT + 5
     RUN_WIN = USEREVENT + 6
